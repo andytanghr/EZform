e8e6bffa59a76136e50efd8cd76da7e6f90bd271
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-05-19 12:52:06 -0500

    finished scrapping form text

---
+++ TXDLRenewalFormCoords.py
@@ -0,0 +1,30 @@
+form_dictionary_en = \
+{
+    'form': 'DL43', # Application for Renewal/Replacement/Change of a Texas DL or ID card
+    'data': [
+        # applicant information
+        { 'x': 23, 'y': 20, 'w': 77, 'h': 5.5, 'value': 'testing another iterating' },
+        { 'x': 108, 'y': 146, 'w': 23, 'h': 5.5, 'value': '1450' },
+        { 'x': 133, 'y': 146, 'w': 22, 'h': 5.5, 'value': 'iterating make another' },
+
+        # contact information
+        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
+        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
+
+        # address information
+        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
+        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
+
+        # questions 1-10
+        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
+        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
+        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
+        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
+
+        # questions 11-17
+        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
+        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
+        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
+
+    ]
+}
