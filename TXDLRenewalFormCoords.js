9bfe414880d4d312740a1e08e78719628893f80e
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-05-19 14:59:14 -0500

    removed random js file

--- TXDLRenewalFormCoords.js
+++
@@ -1,9 +0,0 @@
-form_dictionary = \
-{
-    'form': 'ACCT',
-    'data': [
-        { 'x': 13, 'y': 146, 'w': 94, 'h': 5.5, 'value': 'testing another iterating' },
-        { 'x': 108, 'y': 146, 'w': 23, 'h': 5.5, 'value': '1450' },
-        { 'x': 133, 'y': 146, 'w': 22, 'h': 5.5, 'value': 'iterating make another' },
-    ]
-}
